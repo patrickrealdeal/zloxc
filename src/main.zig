416a65738f91c1ee76ee29c7d86360f997384b36
patrickrealdeal <patrick.persico95@gmail.com>
2023-10-07 16:07:19 +0200

    testing from ubuntu

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
+    std.debug.print("Hello, World!\n", .{});
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
